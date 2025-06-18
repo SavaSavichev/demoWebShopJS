2a0bed5516a692dd86f4e2016b26da8c8f04d15a
Artem <artem.savichev@mail.ru>
2025-06-18 16:28:50 +1200

    added wishlist tests

--- cypress/e2e/pomTests/wishlistPageTests.cy.js
+++ cypress/e2e/pomTests/wishlistPageTests.cy.js
@@ -1,6 +1,7 @@
 /// <reference types="cypress" />
 
 import MainPage from "../../pageObjects/MainPage";
+import wishlistPageData from "../../fixtures/pom/wishlistPageData.json"
 
 describe("Wishlist Page Tests", () => {
     const mainPage = new MainPage();
@@ -12,6 +13,30 @@ describe("Wishlist Page Tests", () => {
             .clickSmartPhoneIcon()
             .clickAddToWishlistButton()
             .header.clickWishlist()
-            .checkProductName("Smartphone");
+            .checkProductName(wishlistPageData.productName);
+    });
+
+    it("Delete item from wishlist", () => {
+        
+        mainPage
+            .header.clickCameraPhotoLinkTopMenu()
+            .clickSmartPhoneIcon()
+            .clickAddToWishlistButton()
+            .header.clickWishlist()
+            .clickRemoveCheckBox()
+            .clickUpdateWishlistButton()
+            .checkEmptyMessageText(wishlistPageData.messageText);
+    });
+
+    it("Add item from wishlist to cart", () => {
+        
+        mainPage
+            .header.clickCameraPhotoLinkTopMenu()
+            .clickSmartPhoneIcon()
+            .clickAddToWishlistButton()
+            .header.clickWishlist()
+            .clickAddCartCheckBox()
+            .clickAddToCartButton()
+            .getProductName().should("have.text", wishlistPageData.productName);
     });
 });
--- cypress/pageObjects/WishlistPage.js
+++ cypress/pageObjects/WishlistPage.js
@@ -1,3 +1,4 @@
+import CartPage from "./CartPage";
 import Header from "./Header";
 import SideMenu from "./SideMenu";
 
@@ -9,11 +10,46 @@ class WishlistPage {
     }
 
     getProductName = () => cy.get("td.product");
+    getRemoveCheckBox = () => cy.get("[name='removefromcart']");
+    getUpdateWishlistButton = () => cy.get("input[value='Update wishlist']");
+    getAddToCartCheckBox = () => cy.get("td.add-to-cart");
+    getAddToCartButton = () => cy.get("input[value='Add to cart']");
+    getEmptyMessage = () => cy.get(".wishlist-content");
 
     checkProductName(productName) {
         this.getProductName().invoke("text").then((text) => {
             expect(productName).to.eq(text.trim());
         });
+    }
+
+    clickRemoveCheckBox() {
+        this.getRemoveCheckBox().click();
+
+        return this;
+    }
+
+    clickUpdateWishlistButton() {
+        this.getUpdateWishlistButton().click();
+
+        return this;
+    }
+
+    checkEmptyMessageText(messageText) {
+        this.getEmptyMessage().invoke("text").then((text) => {
+            expect(messageText).to.eq(text.trim());
+        }); 
+    }
+
+    clickAddCartCheckBox() {
+        this.getAddToCartCheckBox().click();
+
+        return this;
+    }
+    
+    clickAddToCartButton() {
+        this.getAddToCartButton().click();
+
+        return new CartPage();
     }
 }
 
