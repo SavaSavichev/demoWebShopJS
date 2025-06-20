f5c95853d679470e2e77ed62a13ca871f7a7fcb6
Artem <artem.savichev@mail.ru>
2025-06-21 11:31:18 +1200

    added global time out

--- cypress.config.js
+++ cypress.config.js
@@ -1,14 +1,16 @@
 const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
+  defaultCommandTimeout: 10000,
+  pageLoadTimeout: 60000,
+  responseTimeout: 30000,
+  retries: {
+    runMode: 2,
+    openMode: 0,
+  },
   e2e: {
     baseUrl: 'https://demowebshop.tricentis.com/',
-    setupNodeEvents(on, config) {},
+    setupNodeEvents(on, config) {
+    },
   },
 });
-
-// export default defineConfig({
-//   defaultCommandTimeout: 10000,
-//   pageLoadTimeout: 60000,
-//   responseTimeout: 30000,
-// });
